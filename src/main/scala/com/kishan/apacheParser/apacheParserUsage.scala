42f22e18bf38421b06de11b5c4381a0fd610c83d
Kishan <suriya.kishan@live.com>
2019-10-20 12:45:58 +0530

    Added Exception Handleing

--- src/main/scala/com/kishan/apacheParser/apacheParserUsage.scala
+++ src/main/scala/com/kishan/apacheParser/apacheParserUsage.scala
@@ -1,31 +1,47 @@
 package com.kishan.apacheParser
 
-import org.apache.commons.cli.{DefaultParser, Options}
+import org.apache.commons.cli._
 
 object apacheParserUsage {
+
   def main(args: Array[String]): Unit = {
 
     //Definition for the parser
     val options = new Options
-    options.addOption("n", "name", true, "Name of the student")
+    options.addRequiredOption("n", "name", true, "Name of the student")
     options.addOption("a", "age", true, "Age of the student")
 
-    //Parsing the arguments
-    val parser = new DefaultParser
-    val parsedArguments = parser.parse(options,args)
+    try {
+      //Parsing the arguments
+      val parser = new DefaultParser
+      val parsedArguments = parser.parse(options, args)
 
-    //Interrogation of arguments
-    if (parsedArguments.hasOption("name")){
-      println(s"Value in the name argument is: ${parsedArguments.getOptionValue("name")}")
-    } else {
-      println("Name argument not used")
+      //Interrogation of arguments
+      if (parsedArguments.hasOption("name")) {
+        println(s"Value in the name argument is: ${parsedArguments.getOptionValue("name")}")
+      } else {
+        println("Name argument not used")
+      }
+
+      if (parsedArguments.hasOption("age")) {
+        println(s"Value in the age argument is: ${parsedArguments.getOptionValue("age")}")
+      } else {
+        println("Age argument not used")
+      }
+    } catch {
+      case m: MissingOptionException => {
+        println(s"Required Missing Arguments: ${m.getMissingOptions}")
+        help(getClass.getName, options)
+      }
+      case a: MissingArgumentException => {
+        println(s"Missing Argument for the options: ${a.getOption}")
+        help(getClass.getName, options)
+      }
     }
+  }
 
-    if (parsedArguments.hasOption("age")){
-      println(s"Value in the age argument is: ${parsedArguments.getOptionValue("age")}")
-    } else {
-      println("Age argument not used")
-    }
-
+  def help(appname: String, appOptions: Options): Unit = {
+    val helpFormatted = new HelpFormatter
+    helpFormatted.printHelp(appname, appOptions)
   }
 }
